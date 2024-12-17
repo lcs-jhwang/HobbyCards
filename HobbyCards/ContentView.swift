784ea7eb22339c1c1ae4ebcebc951444b1402243
Julien Hwang <jhwang@lcs.on.ca>
2024-12-17 08:51:08 -0500

    Initial Commit

---
+++ HobbyCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HobbyCards
+//
+//  Created by Julien Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HobbyCards/HobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyCardsApp.swift
+//  HobbyCards
+//
+//  Created by Julien Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
